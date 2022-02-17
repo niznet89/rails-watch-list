eb11068a72d5f724c1e5bce2f228215b0e352d30
Tenzin Rose <tenzinrose@gmail.com>
2022-02-17 16:54:27 +0000

    working app, now adding styling

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   get 'bookmarks/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists do
-    resources :bookmarks, only: [:new, :show]
+    resources :bookmarks, only: [:new, :show, :create]
   end
 end
